1cc324220ee9874e5b4aecced95a23f2222a12ef
karan28598 <mr.karanagrawal@gmail.com>
2018-04-16 16:04:01 +0530

    modify project

--- DSAL/hotel.c
+++ DSAL/hotel.c
@@ -1,18 +1,73 @@
 #include<stdio.h>
-#include<string.h>
-#define size 20
+#include<stdlib.h>
 
-//Functions Used
-void insertq(int[],int);
-void deleteq(int[]);
-void display(int[]);
+struct Node{
+  int data;
+  struct Node* next;
+};
 
-//Global Variables
-int front = -1;
-int rear = -1;
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+void enqueue(int x){
+  struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+  temp->data = x; 
+  temp->next = NULL;
+  if(front == NULL && rear == NULL){
+    front = rear = temp;
+    return;
+  }
+  rear->next = temp;
+  rear = temp;
+}
+
+void dequeue(){
+  struct Node* temp = front;
+  if(front == NULL){
+    printf("Queue is Empty\n");
+    return;
+  }
+  if(front == rear){
+    front = rear = NULL;
+  }
+  else{
+    front = front->next;
+  }
+  free(temp);
+}
+
+void display(){
+  struct Node* temp = front;
+  while(temp != NULL){
+    printf("%d ",temp->data);
+    temp = temp->next;
+  }
+  printf("\n");
+}
+
+int correct(int p){
+    if(p>100 && p<131){
+        return 1;
+    }
+    printf("Please select a valid number.\n");
+    return 0;
+}
+
+int search(int p){
+  printf("Checking Availability...\n");
+  struct Node* temp = front;
+  while(temp != NULL){
+    if(temp->data == p){
+        printf("This room is already booked.\n");
+        return 0;
+    }
+    else temp = temp->next;
+  }
+  return 1;
+}
 
 int main(){  
-  int n,ch,age,queue[size],gg;
+  int n,ch,age,gg,status;
   long long phone_number;
   char name[100];
   do{
@@ -24,77 +79,33 @@ int main(){
   scanf("%lld",&phone_number);
   printf("\t\t\tWelcome to Hotel Paradise\t\t\t \n");
   printf("\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~\t\t\t\n");
-  printf("\nChoose from the following numbers to book a room!\n100-110 -> Standard Rooms\n200-210 -> Deluxe Rooms\n300-310 -> Suite ");
+  printf("\nChoose from the following numbers to book a room!\n101-110 -> Standard Rooms\n111-120 -> Deluxe Rooms\n121-130 -> Suite ");
   do{
     printf("\nChoose your action \n1.Book a room\n2.Delete a room\n3.Display status of room booked\n4.Exit\n");
     scanf("%d",&ch);
     switch(ch){
       case 1:printf("Enter room number you want to book:");
              scanf("%d",&n);
-             insertq(queue,n);
+             if(correct(n)){
+                 if(search(n)){
+                    enqueue(n); 
+                    printf("Room no. %d has been booked for you.\n", n);  
+                 }
+             }
              break;
              
-      case 2:deleteq(queue);
+      case 2:dequeue();
              break;
              
-      case 3:display(queue);
+      case 3:printf("\nList of the rooms booked:\n ");
+             display();
              break;
     }
   }
   while(ch!=4);
-  printf("Do you want to book rooms under other user?(1:yes  2:no)\n");
-  scanf("%d",&gg);
+  printf("Do you want to book rooms under other user?(1.Yes  2.No)\n");
+  scanf("%d", &gg);
   }
   while(gg!=2);
   return 0;
-}
-
-//Function Definitions
-void insertq(int queue[],int item){
-  if ((front == 0 && rear == size-1)||(front == rear+1)){
-      printf("Sorry you cannot book room.Rooms are full\n");
-      return;
-  }
-  else if(rear == -1){
-      rear++;
-      front++;
-  }
-  else if(rear == size-1 && front>0){
-      rear =0;
-  } 
-  else{
-      rear++;
-  }
-queue[rear]=item;
-}
-
-void display(int queue[]){
-  int i;
-  printf("Your room has been booked.You booked room number: ");
-  if(front>rear){
-    for(i=front;i<size;i++){
-        printf("%d\t",queue[i]);
-    }
-    for(i=0;i<=rear;i++)
-    printf("%d\t",queue[i]);
-  }
-  else{
-    for(i=front;i<=rear;i++)
-    printf("%d\t",queue[i]);
-  }
-}
-
-void deleteq(int queue[]){
-  if(front==-1){
-      printf("No rooms booked for deletion\n");
-  }
-  else if(front==rear){
-      printf("\nRoom number %d has been cancelled\n",queue[front]);
-      front=-1;
-      rear=-1;
-  }
-  else{
-      printf("\nRoom number %d has been cancelled", queue[front]);
-      front++;
-  }
 }
